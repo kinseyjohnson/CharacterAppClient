ac15ef7263643f1fcb15dce057ebbc3bee174d8f
Freya Black <freyablackdev@gmail.com>
2021-07-30 11:48:17 -0400

    updated landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Landing from './Components/LandingPage';
 import Signup from './Components/Auth/Sign-Log/Signup';
-import Login from './Components/Auth/Sign-Log/Login'
+import Login from './Components/Auth/Sign-Log/Login';
+import Create from './Components/Character/CharCreate';
+import Auth from './Components/Auth/Auth'
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+  const Tog = () => setToggle(!toggle)
 
   // const protectedViews = () => {
   //   return (sessionToken === localStorage.getItem('token') ? <WorkoutIndex token={sessionToken}/>
@@ -13,9 +17,12 @@ function App() {
 
   return (
     <div>
+      {/* <Create/> */}
       <Landing />
-      <Signup style={{alignText: 'center'}}/>
-      <Login style={{alignText: 'center'}}/>
+      <button onClick={Tog} className='button'>Start Here!</button>
+      {toggle ? <Auth/> : null}
+      {/* <Signup style={{alignText: 'center'}}/> */}
+      {/* <Login style={{alignText: 'center'}}/> */}
     </div>
   );
 }
--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './landingpage.css'
-
+import Auth from './Auth/Auth'
 
 const Landing = () => {
+    const [toggle, setToggle] = useState(false);
+    const Tog = () => setToggle(!toggle)
 
     return(
         <div className='mainDiv'>
         <h1 className='h2'>Welcome to the Tavern of the Azure Keys!</h1>
         <p className='p'>Here you can create and store characters for your upcoming TRPG games, or just for a bit of fun!</p>
-            <button className='button'>Start Here!</button>
+            {/* <button onClick={Tog} className='button'>Start Here!</button>
+            {toggle ? <Auth/> : null} */}
         </div>
     )
 }
