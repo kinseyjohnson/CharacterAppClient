bcc9924513bcf62c235396dd6543882264aad421
Freya Black <freyablackdev@gmail.com>
2021-08-05 11:47:57 -0400

    fixing2

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch(window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3000';
         break;
-        case 'https://fab-character-app-client.herokuapp.com/':
+        case 'https://fab-character-app-client.herokuapp.com':
             APIURL = 'https://fab-character-app-server.herokuapp.com'
 }
 
