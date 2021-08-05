94943d04ee74202d674494e5b391b60fdf693c02
Freya Black <freyablackdev@gmail.com>
2021-08-05 11:45:36 -0400

    fixing

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,14 +1,10 @@
 let APIURL = '';
 
 switch(window.location.hostname) {
-    // this is the local host name of your react app
     case 'localhost' || '127.0.0.1':
-        // this is the local host name of your API
         APIURL = 'http://localhost:3000';
         break;
-        // this is the deployed react application
-        case 'https://git.heroku.com/fab-character-app-client.git':
-            // this is the full url of your deployed API
+        case 'https://fab-character-app-client.herokuapp.com/':
             APIURL = 'https://fab-character-app-server.herokuapp.com'
 }
 
